a8648d1d961bcafd700c1d47798860b712559e57
Katherine Thongsakounh & Nicole Cody <pair+kthong3.ncody@devbootcamp.com>
2017-11-09 12:07:47 -0600

    add view for user show and update nav bar links

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
   has_secure_password
 
   has_many  :pinned_recipes
