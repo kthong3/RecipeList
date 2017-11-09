c22a74073eb11fbf6985b5f7cffae465bbb31413
Katherine Thongsakounh & Nicole Cody <pair+kthong3.ncody@devbootcamp.com>
2017-11-09 12:20:38 -0600

    add sessions helper methods and update nav links with helps in layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+      log_in(@user)
       redirect_to user_path(@user)
     else
       render 'new'
